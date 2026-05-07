1d9e7a149ae99b9cdd34b06fddd20286f228cd0e
“Nilde <nilde.benedetti@gmail.com>
2026-05-07 17:08:29 +0200

    add: card jsx in Card function - Card.jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,32 @@
+{/** 
+    {id},
+    {fullName},
+    {birthYear},
+    {bio},
+    {img},
+    {awards}
+           */}
+
+
+function Card({
+    img = "link",
+    fullName = "Name Surname",
+    birthYear = "YYYY",
+    bio = "Something about her life that made her the Woman she is.",
+    awards = "# Oscars, # Academy Awards"
+
+
+}
+) {
+
+    return <div className="card m-3" >
+  <img src={img} className="card-img-top" alt={fullName}></img>
+  <div className="card-body">
+    <h5 className="card-title">{fullName}</h5>
+    <p className="card-text">`Born in ${birthYear}.`</p>
+    <p className="card-text">`Awards won: ${awards}.`</p>
+    <p className="card-text">{bio}</p>
+  </div>
+</div>
+}
+export default Card;
