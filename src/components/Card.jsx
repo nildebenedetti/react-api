fb5f4f64c5455e382eb7f5bc22d06bc465ad6c4c
“Nilde <nilde.benedetti@gmail.com>
2026-05-08 00:26:49 +0200

    Add: prop della card con paragraph per visualizzare línfo dei film famosi

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,18 +13,18 @@ function Card({
     fullName = "Name Surname",
     birthYear = "YYYY",
     bio = "Something about her life that made her the Woman she is.",
-    awards = "# Oscars, # Academy Awards"
-
-
+    awards = "# Oscars, # Academy Awards",
+    moviesStarred = "'TFinding Nemo', 'Kung Fu Panda', 'Lion King'"
 }
 ) {
 
-    return <div className="card m-3 p-2 col">
+    return <div className="card m-3 col">
   <img src={img} className="card-img-top" alt={fullName}></img>
   <div className="card-body">
     <h5 className="card-title">{fullName}</h5>
     <p className="card-text">{`Born in ${birthYear}.`}</p>
     <p className="card-text">{`Awards won: ${awards}.`}</p>
+    <p className="card-text">{`You might have seen their face in ${moviesStarred }`}</p>
     <p className="card-text">{bio}</p>
   </div>
 </div>
