137e6943f4b3366597de995226896fa6e1490bce
“Nilde <nilde.benedetti@gmail.com>
2026-05-08 00:29:22 +0200

    scherzetto... fix del card body - ora posso dormire in pace

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,24 +9,24 @@
 
 
 function Card({
-    img = "link",
-    fullName = "Name Surname",
-    birthYear = "YYYY",
-    bio = "Something about her life that made her the Woman she is.",
-    awards = "# Oscars, # Academy Awards",
-    moviesStarred = "'TFinding Nemo', 'Kung Fu Panda', 'Lion King'"
+  img = "link",
+  fullName = "Name Surname",
+  birthYear = "YYYY",
+  bio = "Something about her life that made her the Woman she is.",
+  awards = "# Oscars, # Academy Awards",
+  moviesStarred = "'TFinding Nemo', 'Kung Fu Panda', 'Lion King'"
 }
 ) {
 
-    return <div className="card m-3 p-3 col">
-  <img src={img} className="card-img-top" alt={fullName}></img>
-  <div className="card-body">
-    <h5 className="card-title">{fullName}</h5>
-    <p className="card-text">{`Born in ${birthYear}.`}</p>
-    <p className="card-text">{`Awards won: ${awards}.`}</p>
-    <p className="card-text">{`You might have seen their face in ${moviesStarred }`}</p>
-    <p className="card-text">{bio}</p>
+  return <div className="card m-3 p-3 col">
+    <img src={img} className="card-img-top" alt={fullName}></img>
+    <div className="card-body">
+      <h5 className="card-title">{fullName}</h5>
+      <p className="card-text">{`Born in ${birthYear}.`}</p>
+      <p className="card-text">{bio}</p>
+      <p className="card-text">{`Awards won: ${awards}.`}</p>
+      <p className="card-text">{`You might have seen their face in ${moviesStarred}`}</p>
+    </div>
   </div>
-</div>
 }
 export default Card;
