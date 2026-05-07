282c9f32bd564f85fe900b005108418bae0d6db5
“Nilde <nilde.benedetti@gmail.com>
2026-05-07 15:27:47 +0200

    add: API URL fetch + console log in App.jsx (funziona!!)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ function App() {
 
   const API_URL = 'https://lanciweb.github.io/demo/api/actresses/';
 
+  fetch(API_URL)
+    .then (response => {
+      return response.json();
+    })
+    .then (data => 
+      console.log(data),
+    )
+    .catch (error =>
+      console.error('probleminooo',error)
+    );
+
   return <div>
 
   </div>
