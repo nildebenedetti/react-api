4406215140d55046d5c764cb7ac0d4719dcbe506
“Nilde <nilde.benedetti@gmail.com>
2026-05-07 16:09:16 +0200

    add: commenti al processo in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,37 @@ import { useEffect } from "react";
 
 
 function App() {
-
+  // salvo API URL
   const API_URL = 'https://lanciweb.github.io/demo/api/actresses/';
-
+  // variabile di stato per Actressess data
   const [ActressesData, setActressesData] = [];
 
-
+  // funzione per creare fetch dei dati
   const getActressesData = () => {
     return fetch(API_URL)
 
-    .then(response => {
-      return response.json();
-    })
-    .then(json => {
-    const data = json;
-    return data;
-    })
-    .catch(error =>
-      console.error('probleminooo', error)
-    )};
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        const data = json;
+        return data;
+      })
+      .catch(error =>
+        console.error('probleminooo', error)
+      )
+  };
 
-    const clickHandler = () => {
-      console.log('click!');
-      getActressesData()
+  const clickHandler = () => {
+    // richiamo la variabile salvata in getActressessData
+    getActressesData()
       .then(data => {
         console.log(data);
+        // adesso che li ho, devo settare lo stato mettendo 
+        // i dati organizzati come voglio io
       });
-      
-    }
+
+  }
 
   return <div>
     <ul>
